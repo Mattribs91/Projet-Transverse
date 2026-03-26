1aa1560c87ba725ed44bd811cbc71edafd1a5f96
Matthéo Ribeiro <mattheoribeiro91@hotmail.com>
2026-03-26 09:55:43 +0100

    Update Maven config with JUnit and convert AvisTest to a real JUnit test

--- Code Java/Trackr/tests/trackrtest/AvisTest.java
+++ Code Java/Trackr/tests/trackrtest/AvisTest.java
@@ -1,57 +1,15 @@
 package trackrtest;
 
-import java.util.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import trackr.Avis;
+import java.util.Date;
 
 public class AvisTest {
 
-	UserTest createur;
-	MediaTest mediaAssocie;
-	private Date dateDeCreation;
-	private String commentaire;
-	private int nombreEtoiles;
-
-	public AvisTest(UserTest createur, MediaTest mediaAssocie, Date dateDeCreation, String commentaire, int nombreEtoiles) {
-		this.createur = createur;
-		this.mediaAssocie = mediaAssocie;
-		this.dateDeCreation = dateDeCreation;
-		this.commentaire = commentaire;
-		this.nombreEtoiles = nombreEtoiles;
+	@Test
+	public void testGetNombreEtoiles() {
+		Avis avis = new Avis(null, null, new Date(), "Super film", 5);
+		assertEquals(5, avis.getNombreEtoiles(), "Le nombre d'étoiles doit être 5");
 	}
-
-	public UserTest getCreateur() { return this.createur; }
-
-	public void setCreateur(UserTest createur) { this.createur = createur; }
-
-	public MediaTest getMediaAssocie() {
-		return this.mediaAssocie;
-	}
-
-	public void setMediaAssocie(MediaTest mediaAssocie) {
-		this.mediaAssocie = mediaAssocie;
-	}
-
-	public Date getDateDeCreation() {
-		return this.dateDeCreation;
-	}
-
-	public void setDateDeCreation(Date dateDeCreation) {
-		this.dateDeCreation = dateDeCreation;
-	}
-
-	public String getCommentaire() {
-		return this.commentaire;
-	}
-
-	public void setCommentaire(String commentaire) {
-		this.commentaire = commentaire;
-	}
-
-	public int getNombreEtoiles() {
-		return this.nombreEtoiles;
-	}
-
-	public void setNombreEtoiles(int nombreEtoiles) {
-		this.nombreEtoiles = nombreEtoiles;
-	}
-
 }
